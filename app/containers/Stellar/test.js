bc3c544946f51e0e471d98ad55d600f8cc71a52d
Régis Guyomarch <regisg@gmail.com>
2016-08-28 04:00:30 +0200

    stellar + suite mep normalizr

--- app/containers/Stellar/test.js
+++ app/containers/Stellar/test.js
@@ -3,5 +3,5 @@ const api = require('../../utils/stellarApi');
 api.loadAccount('GCRN5SVVM72CYTBS3RM4GNDYBJFXR23DW6MRJZKDUQFAFIWNSGJDCAOV')
   .then(account => ({ account }))
   .catch(err => {
-    console.log(err);
+    throw new Error(err);
   });
