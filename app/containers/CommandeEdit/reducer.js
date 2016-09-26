b9784833341a4bb7c40d8230f85a064e5e80eff6
Régis Guyomarch <regisg@gmail.com>
2016-09-26 09:54:46 +0200

    ajout initialState

--- app/containers/CommandeEdit/reducer.js
+++ app/containers/CommandeEdit/reducer.js
@@ -8,7 +8,17 @@ import {
   DEFAULT_ACTION,
 } from './constants';
 
-const initialState = {};
+const initialState = {
+  commandeId: null,
+  dateLivraison: null,
+  datePaiement: null,
+  livraisonId: null,
+  modifiee: false,
+  montant: null,
+  prestationRelai: null,
+  recolteFond: null,
+  utilisateurId: null,
+};
 
 function commandeEditReducer(state = initialState, action) {
   switch (action.type) {
